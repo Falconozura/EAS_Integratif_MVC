1715435c69dfbf3453785c8549730ef6b9af63de
Falconozura <57929995+Falconozura@users.noreply.github.com>
2020-05-18 13:55:50 +0700

    Create Constants.php

---
+++ integratif/fp/app/core/Constants.php
@@ -0,0 +1,7 @@
+<?php
+
+    define("BASE_URL", 'http://localhost/integratif/fp/public');
+    define("DB_HOST", "localhost");
+    define("DB_NAME", "fp");
+    define("DB_USERNAME", "root");
+    define("DB_PASSWORD", "");
